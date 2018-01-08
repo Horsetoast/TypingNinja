f7d4346981b6adbb0db92161f1a863486fb28f39
Matus <peciar.matus@gmail.com>
2018-01-08 15:26:48 +0800

    Words list update and words per level changed

--- app/config.js
+++ app/config.js
@@ -3,7 +3,7 @@ export default {
   WORD_MAX_SPAWN_SPEED: 2000, // In milliseconds
   WORD_MIN_LIFESPAN: 7000, // In milliseconds
   WORD_MAX_LIFESPAN: 10000, // In milliseconds
-  WORDS_PER_LEVEL: 10,
+  WORDS_PER_LEVEL: 30,
   CONTAINER_WORDS_OFFSET: 100, // In pixels,
   LIVES: 6
 };
